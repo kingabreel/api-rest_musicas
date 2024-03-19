3ad2a76f98c1bb9cce8336bb9f0fbc8e4a188d69
kingabreel <gabrielyury441@gmail.com>
2024-03-19 00:07:59 -0300

    config: pom

---
+++ src/main/java/one/digital/arqhexagonal/ArqHexagonalApplication.java
@@ -0,0 +1,13 @@
+package one.digital.arqhexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqHexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArqHexagonalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/one/digital/arqhexagonal/ArqHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digital.arqhexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqHexagonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
