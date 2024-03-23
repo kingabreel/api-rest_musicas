0acd3e078116065bb301ce4edee278f01d833e20
kingabreel <gabrielyury441@gmail.com>
2024-03-23 10:52:00 -0300

    fix: sql syntax error

--- src/main/resources/db/migration/V20240322232100__create_music_table.sql
+++ src/main/resources/db/migration/V20240322232100__create_music_table.sql
@@ -1,8 +1,8 @@
-create table music {
+CREATE TABLE music (
     id bigserial not null,
     titulo varchar(50),
     autor varchar(50),
     album varchar(50),
-    genero varchar(20)
+    genero varchar(20),
     primary key (id)
-}
+    );
