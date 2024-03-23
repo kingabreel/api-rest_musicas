4a4d002a8df74813143c8f2590fc25a2a287f1b7
kingabreel <gabrielyury441@gmail.com>
2024-03-23 10:52:17 -0300

    fix: profile name

--- start_api.sh
+++ start_api.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./mvnw spring-boot:run -Dspring-boot.run.profiles=dev -Dspring-boot.run.jvmArguments="-Xmx256m -Xms128m" -Dspring-boot.run.arguments="'--DB_USER=dio.music' '--DB_PASSWORD=dio.music' '--DB_NAME=musics'"
+./mvnw spring-boot:run -Dspring-boot.run.profiles=dev -Dspring-boot.run.jvmArguments="-Xmx256m -Xms128m" -Dspring-boot.run.arguments="'--DB_USER=dio.avenger' '--DB_PASSWORD=dio.avenger' '--DB_NAME=avengers'"
